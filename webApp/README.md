9b766a135327bdf2be0516de8d832306cc5ceee5
Terrynyam <terrynyamz93@gmail.com>
2022-07-12 16:18:45 +0200

    new file

--- webApp/README.md
+++
@@ -1,6 +0,0 @@
-# JuniorDeveloper_Test_Scandiweb
-
-
-## Description
-
-This is a Scandiweb Junior Web Dev task done using OOP Php and MySQL
--- webApp/app/init.php
+++
@@ -1,5 +0,0 @@
-<?php 
-require "./app/core/app.php";
-require "./app/core/controller.php";
-require "./app/models/DbConn.php";
-require "./app/models/Product.php";
